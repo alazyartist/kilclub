4225ecdfa5d84433eb2502fd2535629906cf1469
tohzt <steven.g.french@live.com>
2023-09-04 17:21:04 -0700

    flex style guide

--- src/pages/testing.tsx
+++ src/pages/testing.tsx
@@ -15,10 +15,10 @@ export default function Home() {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Keep <span className="text-[hsl(280,100%,70%)]">it</span> Local
           </h1>
-          <div className="flex-row -center gap-8">
+          <div className="flex-col -center gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/testing"
+              href="/test"
             >
               <h3 className="text-2xl font-bold">Button One</h3>
               <div className="text-lg">----------</div>
