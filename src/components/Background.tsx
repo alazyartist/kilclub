7c7dc8cdd7bddf0f6271f5024b75f34cf1f2fcea
tohzt <steven.g.french@live.com>
2023-09-05 01:00:13 -0700

    minor style chanegs

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Background = ({ children }: React.PropsWithChildren) => {
   return (
-    <div className="bg-base-light flex min-h-screen flex-col items-center justify-center">
+    <div className="bg-base-light flex-col">
       {children}
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-base-light flex min-h-screen flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <div className="-center flex-col space-y-2">
-            <div className="-center flex-row gap-4">
+      <main>
+        <div className="container flex-coll -center">
+          <div className="flex-coll -center space-y-2 p-4">
+            <div className="flex-row -center gap-4">
               <label className="text-primary-dark font-bold">Zip Code: </label>
               <input
                 className="border-primary-dark border-2"
@@ -24,12 +24,12 @@ export default function Home() {
                   }
                 }}
               />
-              <button className="bg-primary-dark rounded-full px-1 py-1 text-xl font-bold text-white">
-                [?]
+              <button className="bg-primary-dark rounded-full px-2 py-1 text-m font-bold text-white">
+                Search
               </button>
             </div>
             <Link
-              className="bg-accent-dark flex max-w-xs flex-col gap-4 rounded-xl p-4 text-white hover:bg-white/20"
+              className="w-full bg-accent max-w-xs flex-coll rounded-xl p-4 text-white hover:bg-accent-dark"
               href="/testing"
             >
               <h3 className="text-2xl font-bold text-[accent-light]">
@@ -38,18 +38,14 @@ export default function Home() {
               <div className="text-lg">----------</div>
             </Link>
             <Link
-              className="bg-accent flex max-w-xs flex-col gap-4 rounded-xl p-4 text-white hover:bg-white/20"
-              href="/test"
+              className="w-full bg-accent max-w-xs flex-coll rounded-xl p-4 text-white hover:bg-accent-dark"
+              href="/"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">Button Two</h3>
               <div className="text-lg">----------</div>
             </Link>
           </div>
-          <UserButton />
-          <SignedOut>
-            <SignIn />
-          </SignedOut>
         </div>
       </main>
     </>
