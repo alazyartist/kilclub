0ea492e5530be45ca8838e1294aceaafad6aff6c
Dylan James <alazyartist@gmail.com>
2023-09-07 00:05:21 -0600

    verified webhooks

--- src/pages/api/webhooks/stripe.ts
+++ src/pages/api/webhooks/stripe.ts
@@ -1,14 +1,35 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { buffer } from "stream/consumers";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
 type ResponseData = {};
 const stripe = require("stripe")(env.STRIPE_SECRET_KEY);
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default async function stripehandler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  console.log(req.body);
+  let event;
+  const body = await buffer(req);
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body.toString(),
+      req.headers["stripe-signature"],
+      env.STRIPE_WEBHOOK_SECRET,
+    );
+    console.log(event.type);
+    res.status(200).end();
+  } catch (err) {
+    console.log(err);
+    console.log("Webhook Verification Failed");
+    return res.status(400).end();
+  }
   //stripe go here
-  res.status(200).end();
 }
