7e49902e52a8e3a481deb3b7ddb42205d71ae50e
Dylan James <alazyartist@gmail.com>
2023-09-08 01:45:37 -0600

    fix buttons

--- src/pages/upgrade/index.tsx
+++ src/pages/upgrade/index.tsx
@@ -3,19 +3,21 @@ import React from "react";
 
 const UpgradeFlow = () => {
   return (
-    <div className="flex-coll h-full w-full items-center gap-2">
+    <div className="flex-coll -center space-y-2 p-4">
       <h1 className="pt-14 text-2xl font-bold lg:text-5xl">I am a</h1>
       <Link
-        className="bg-accent-light w-[200px] rounded-md p-2 text-center text-white lg:text-3xl"
+        className="w-full max-w-[320px] rounded-xl bg-accent p-4 text-white lg:text-3xl"
         href="/upgrade/customer"
       >
-        Customer
+        <h3 className="text-2xl font-bold">Customer</h3>
+        <div className="text-lg">----------</div>
       </Link>
       <Link
-        className="bg-accent-light w-[200px] rounded-md p-2 text-center text-white lg:text-3xl"
+        className="w-full max-w-[320px]  rounded-xl bg-accent p-4 text-white lg:text-3xl"
         href="/upgrade/business"
       >
-        Business
+        <h3 className="text-2xl font-bold">Business</h3>
+        <div className="text-lg">----------</div>
       </Link>
     </div>
   );
