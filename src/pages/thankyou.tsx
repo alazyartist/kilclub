fb15a97a79682513e0d7ce1051f594bd67df2007
Dylan James <alazyartist@gmail.com>
2023-09-09 20:57:57 -0600

    thank you redirect

--- src/pages/thankyou.tsx
+++ src/pages/thankyou.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import { api } from "~/utils/api";
 
 const ThankYou = () => {
+  const { data: user } = api.user.getUser.useQuery();
+  const router = useRouter();
+  useEffect(() => {
+    if (user?.isBusiness) {
+      setTimeout(() => {
+        router.push("jobs");
+      }, 4000);
+    } else {
+      router.push("/");
+    }
+  }, [user]);
   return (
     <div className="flex-coll -center">
       <h1 className="text-center text-3xl font-bold">Welcome to the club</h1>
       <p>Thank You for your payment!</p>
+      <p className="text-xs">you will be redirected shortly</p>
     </div>
   );
 };
