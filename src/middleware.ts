cc62a19b73645308cd90103506e8bcb93104d7ef
Dylan James <alazyartist@gmail.com>
2023-09-11 16:29:31 -0600

    allow getBusinesses public

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,12 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks/(.*)", "/testing"],
+  publicRoutes: [
+    "/",
+    "/api/webhooks/(.*)",
+    "/api/trpc/business.getBusinesses",
+    "/testing",
+  ],
 });
 
 export const config = {
