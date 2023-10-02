8b324f16a7b03933122a3aa67e961ce12039c957
Dylan James <alazyartist@gmail.com>
2023-10-01 19:21:30 -0600

    cleaner delete

--- src/components/jobs/JobDetails.tsx
+++ src/components/jobs/JobDetails.tsx
@@ -12,84 +12,92 @@ const JobDetails = ({ job, visible }: { job: Jobs; visible?: boolean }) => {
   return (
     <>
       {image && <ImagePopover image={image} />}
-      <div
-        key={job.job_id}
-        className="relative min-w-[320px] rounded-md bg-accent p-2 text-zinc-100"
-      >
-        {showDetails ? (
-          <>
+      <div>
+        <div
+          key={job.job_id}
+          className="relative min-w-[320px] rounded-md bg-accent p-2 text-zinc-100"
+        >
+          {showDetails ? (
+            <>
+              <div
+                // onClick={() => setShowDetails(false)}
+                className="flex justify-between"
+              >
+                <div>
+                  {job.customer_phone_number}{" "}
+                  <button
+                    onClick={() => {
+                      navigator.share({
+                        url: `http://keep-it-local-club.vercel.app/review?pnid=%2B${job.customer_phone_number.slice(
+                          1,
+                        )}`,
+                        title: `Review ${job.job_id}`,
+                      });
+
+                      console.log("gonna share");
+                    }}
+                  >
+                    share
+                  </button>
+                </div>
+                <div>
+                  <h1 className="text-right text-lg">{job.zip_code}</h1>
+                  <p className="text-right text-xs">
+                    {job.date.toDateString()}
+                  </p>
+                </div>
+              </div>
+              {Array.isArray(job.media) && (
+                <div className="grid h-full w-full grid-cols-3 gap-2">
+                  {job.media.map((img) => {
+                    if (typeof img === "string") {
+                      return (
+                        <div
+                          key={img}
+                          className="relative flex flex-col items-center"
+                        >
+                          <img
+                            onClick={() => router.push(`?image=${img}`)}
+                            alt={`job_detail_${img}`}
+                            className="aspect-square h-full w-full rounded-md object-cover"
+                            src={img}
+                            width={100}
+                            height={100}
+                          />
+                          <DeleteMedia job={job} objectKey={img} />
+                        </div>
+                      );
+                    }
+                  })}
+                </div>
+              )}
+            </>
+          ) : (
             <div
-              // onClick={() => setShowDetails(false)}
+              onClick={() => setShowDetails(true)}
               className="flex justify-between"
             >
-              <div>
-                {job.customer_phone_number}{" "}
-                <button
-                  onClick={() => {
-                    navigator.share({
-                      url: `http://keep-it-local-club.vercel.app/review?pnid=%2B${job.customer_phone_number.slice(
-                        1,
-                      )}`,
-                      title: `Review ${job.job_id}`,
-                    });
-
-                    console.log("gonna share");
-                  }}
-                >
-                  share
-                </button>
-              </div>
-              <div>
-                <h1 className="text-right text-lg">{job.zip_code}</h1>
-                <p className="text-right text-xs">{job.date.toDateString()}</p>
-              </div>
+              <p className="place-self-end text-right text-xs">
+                {job.date.toDateString()}
+              </p>
+              <h1 className="text-right text-lg">{job.zip_code}</h1>
             </div>
-            {Array.isArray(job.media) && (
-              <div className="grid h-full w-full grid-cols-3 gap-2">
-                {job.media.map((img) => {
-                  if (typeof img === "string") {
-                    return (
-                      <div
-                        key={img}
-                        onClick={() => router.push(`?image=${img}`)}
-                        className="flex flex-col items-center"
-                      >
-                        <img
-                          alt={`job_detail_${img}`}
-                          className="aspect-square h-full w-full rounded-md object-cover"
-                          src={img}
-                          width={100}
-                          height={100}
-                        />
-                        <DeleteMedia job={job} objectKey={img} />
-                      </div>
-                    );
-                  }
-                })}
-              </div>
+          )}
+        </div>
+        {showDetails && (
+          <div className=" flex justify-between rounded-md bg-base-light p-2">
+            <UploadMediaForm job_id={job.job_id} />
+            {!job.isCompleted ? (
+              <button
+                onClick={() => markComplete({ job_id: job.job_id })}
+                className="rounded-md bg-zinc-900 bg-opacity-20 p-2"
+              >
+                Mark Complete
+              </button>
+            ) : (
+              <p className="p-2">Completed</p>
             )}
-            <div className="flex justify-between p-2">
-              <UploadMediaForm job_id={job.job_id} />
-              {!job.isCompleted && (
-                <button
-                  onClick={() => markComplete({ job_id: job.job_id })}
-                  className="rounded-md bg-zinc-100 bg-opacity-20 p-2"
-                >
-                  Mark Complete
-                </button>
-              )}
-              <DeleteJob job={job} />
-            </div>
-          </>
-        ) : (
-          <div
-            onClick={() => setShowDetails(true)}
-            className="flex justify-between"
-          >
-            <p className="place-self-end text-right text-xs">
-              {job.date.toDateString()}
-            </p>
-            <h1 className="text-right text-lg">{job.zip_code}</h1>
+            <DeleteJob job={job} />
           </div>
         )}
       </div>
@@ -159,7 +167,7 @@ const DeleteMedia = ({ job, objectKey }: { job: Jobs; objectKey: string }) => {
   return (
     <>
       {deleteCheck ? (
-        <div>
+        <div className="absolute top-5 space-x-3">
           <button
             onClick={() =>
               deleteMedia({ job_id: job.job_id, objectKey: objectKey })
@@ -178,7 +186,7 @@ const DeleteMedia = ({ job, objectKey }: { job: Jobs; objectKey: string }) => {
       ) : (
         <button
           onClick={() => setDeleteCheck(true)}
-          className="rounded-md bg-red-500 p-1 text-xs"
+          className="absolute right-0 top-0 rounded-bl-md rounded-tr-md bg-red-500 p-1 text-xs"
         >
           delete
         </button>
