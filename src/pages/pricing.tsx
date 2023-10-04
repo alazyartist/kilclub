73321283c87459d9eec6015e712b83b1c5b3f767
Dylan James <alazyartist@gmail.com>
2023-10-03 23:45:18 -0600

    pricing page

---
+++ src/pages/pricing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pricing = () => {
+  return (
+    <div className="w-full bg-zinc-300 p-2">
+      <p className="text-center text-xl font-bold">Pricing</p>
+    </div>
+  );
+};
+
+export default Pricing;
