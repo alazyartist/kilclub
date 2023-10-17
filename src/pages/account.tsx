cd7604aed1df8f4571c8ae5b084378525603690a
Dylan James <alazyartist@gmail.com>
2023-10-16 21:42:03 -0600

    subscription details

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -2,13 +2,14 @@ import { UserButton, UserProfile } from "@clerk/nextjs";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import ManageCategories from "~/components/account/ManageCategories";
+import Smiley from "~/components/layout/Icons";
 import { api } from "~/utils/api";
 
 const Account = () => {
   const stripe_customer_id = api.user.getStripeCustomer.useQuery();
 
   return (
-    <div className="flex-coll gap-[0.75rem] p-2 lg:gap-[2rem]">
+    <div className="flex-coll gap-[0.75rem] p-2 pt-8 lg:gap-[2rem]">
       {stripe_customer_id && (
         <SubscriptionDetails
           stripe_customer_id={stripe_customer_id.data as string}
@@ -70,21 +71,33 @@ const SubscriptionDetails = ({
 
   return (
     <div
-      className="flex w-[55rem] min-w-[320px] max-w-[calc(100vw-0.75rem)] justify-between place-self-center rounded-md border-[1px] border-white bg-base-light p-2 lg:max-w-[calc(100vw-5rem)]"
+      className=" w-[55rem] min-w-[320px] max-w-[calc(100vw-0.75rem)] place-self-center rounded-xl border-[1px] border-white bg-base-light p-8 lg:max-w-[calc(100vw-5rem)]"
       style={{ boxShadow: "rgba(0, 0, 0, 0.16) 0px 12px 24px" }}
     >
-      <div className="flex flex-col">
-        <p className="text-xl font-bold text-zinc-900">{tier}</p>
-        <p className="text-xs text-zinc-700">{status}</p>
+      <h1 className="pb-2 text-2xl font-bold">Subscription Details</h1>
+      <div className="flex justify-between text-zinc-900">
+        <div className="flex flex-col gap-2 ">
+          <div className="flex min-w-[320px] place-items-center gap-2 rounded-lg border-[1px] border-white bg-zinc-200">
+            <p className="flex-[2] whitespace-nowrap p-4 ">current plan</p>
+            <p className="flex-1  px-4 font-bold ">{tier}</p>
+          </div>
+          <div className="flex place-items-center gap-2 rounded-lg border-[1px] border-white bg-zinc-200">
+            <p className="flex-[2] p-4 ">status</p>
+            <p className="flex flex-1 place-items-center gap-2 px-4 font-bold ">
+              {status === "active" && <Smiley className={"h-6 w-6"} />}
+              {status}
+            </p>
+          </div>
+        </div>
+        <button
+          onClick={() =>
+            createBillingPortal({ stripe_customer_id: stripe_customer_id })
+          }
+          className="place-self-center rounded-xl bg-accent-light p-2 text-white"
+        >
+          Manage Subscription
+        </button>
       </div>
-      <button
-        onClick={() =>
-          createBillingPortal({ stripe_customer_id: stripe_customer_id })
-        }
-        className="place-self-center rounded-xl bg-accent-light p-2 text-white"
-      >
-        Manage Subscription
-      </button>
     </div>
   );
 };
